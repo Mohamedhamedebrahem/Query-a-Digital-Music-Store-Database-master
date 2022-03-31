6ecf6f927308bdef682d74a667b517606d9a3d87
Mohamed Hamed <96214618+Mohamedhamedebrahem@users.noreply.github.com>
2022-03-31 12:57:55 -0700

    Update Question 1.sql

--- Question 1.sql
+++ Question 1.sql
@@ -1,3 +1,23 @@
-SELECT invoice.*
-FROM invoice
-where invoice.invoicedate between '2013-01-01' and '2014-01-01';
+Query 1 - In Which Year, Which Country Spent The Most Money and How Much in Total? */
+
+WITH CTE AS
+	(
+	SELECT 
+	   c.Country, 
+	   strftime('%Y', i.InvoiceDate) AS Year, 
+	   ROUND(SUM(i.total),2) AS total_sales
+	FROM Invoice AS i
+	JOIN InvoiceLine il
+	ON i.InvoiceId = il.InvoiceId
+	JOIN Customer AS c
+	ON c.CustomerId = i.CustomerId
+	GROUP BY 1,2
+	)
+
+SELECT 
+   Country, 
+   Year,
+   MAX(total_sales)AS Max_Sales
+FROM CTE
+GROUP BY 2
+ORDER BY 2
