f414f5f8c36e58ff5cc26098543633b01ca84538
Mohamed Hamed <96214618+Mohamedhamedebrahem@users.noreply.github.com>
2022-03-31 12:58:38 -0700

    Update Question 2.sql

--- Question 2.sql
+++ Question 2.sql
@@ -1,3 +1,16 @@
-SELECT firstName,lastName,country
- FROM customer
- WHERE country IN("Brazil","Canada","India","Sweden");
+Query 2 Which Genres Have an Average Track Unit Price More Than Overall Average? */
+
+SELECT 
+   g.name AS Genre,
+   AVG(t.UnitPrice) AS "Average Unit Price", 
+   (SELECT ROUND(AVG(unitprice),2) 
+   FROM Track ) AS "Overall Average"
+FROM Track t
+JOIN Genre g
+ON t.GenreId = g.GenreId
+WHERE T.UnitPrice > 
+		(
+		SELECT AVG(unitprice)
+		FROM Track
+		)
+GROUP BY 1
